a4bfcfbb33425e14e8f33dfdc70c366171f0be95
rachic90 <rachic90@gmail.com>
2017-10-21 09:35:41 +0200

    Add files via upload

---
+++ covek.php
@@ -0,0 +1,29 @@
+<?php
+
+Class Covek
+{
+	public $glava;
+	public $ramena;
+	public $ruke;
+	public $noge;
+	
+	public function __construct disanje()
+	{
+		echo "The man is alive while he is breathing.";
+	}
+	
+	public function hodanje()
+	{
+		echo "Walk!";
+	}
+	
+	public function trcanje()
+	{
+		echo "Run";
+	}
+	
+}
+
+
+
+?>
